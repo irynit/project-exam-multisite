07d049db425fdf1255e1942cada279ae862adc68
irynit <irynachmel@gmail.com>
2023-01-20 19:27:51 +0200

    added section, dropdown list

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,10 @@
-console.log("test");
+let arrowClick = document.querySelectorAll('.faqs__row-right-title');
+
+console.log(arrowClick);
+
+for (let i = 0; i < arrowClick.length; i++) {
+  arrowClick[i].onclick = () => {
+    arrowClick[i].classList.toggle('active');
+    arrowClick[i].classList.toggle('arrow');
+  }
+}
