81defeb52654f7ba8bc892c852987ee81d5263ad
irynit <irynachmel@gmail.com>
2023-01-22 20:50:15 +0200

    additional commit

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,10 +1,17 @@
-let arrowClick = document.querySelectorAll('.faqs__row-right-title');
+let arrowFaq = document.querySelectorAll('.faqs__row-right-title');
+let arrowDriving = document.querySelectorAll('.driving__row-right-title');
 
-console.log(arrowClick);
+console.log(arrowFaq);
+console.log(arrowDriving);
 
-for (let i = 0; i < arrowClick.length; i++) {
-  arrowClick[i].onclick = () => {
-    arrowClick[i].classList.toggle('active');
-    arrowClick[i].classList.toggle('arrow');
+for (let i = 0; i < arrowFaq.length; i++) {
+  arrowFaq[i].onclick = () => {
+    arrowFaq[i].classList.toggle('active');
+  }
+}
+
+for (let i = 0; i < arrowDriving.length; i++) {
+  arrowDriving[i].onclick = () => {
+    arrowDriving[i].classList.toggle('active');
   }
 }
